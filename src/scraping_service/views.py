b44e01e523368d17616e0401415b04a5de0914d3
Darya Belousova <dbelousova70@yandex.ru>
2022-11-06 13:16:27 +0400

    5th commit

--- src/scraping_service/views.py
+++ src/scraping_service/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 import datetime
 
+
 def home(request):
     date = datetime.datetime.now().date()
     name = 'Dave'
     _context = {'date': date, 'name': name}
-    return render(request, 'home.html', _context)  # куда передаем значение, и сами значения
+    return render(request, 'base.html', _context)  # куда передаем значение, и сами значения
